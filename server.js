127724d17aff258e018cfd5090f989eff6c19dda
John J Czaplewski <jczaplew@gmail.com>
2015-04-23 11:14:41 -0500

    Clean up and fix

--- server.js
+++ server.js
@@ -1,30 +1,18 @@
 var express = require("express"),
-    bodyParser = require("body-parser"),
-    v1 = require("./v1"),
-    v2 = require("./v2"),
     app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-
-// parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: "application/vnd.api+json" }))
-
 // Load and prefix all routes with /api and appropriate version
-app.use("/api/v1", v1);
-app.use("/api/v2", v2);
+app.use("/api/v1", require("./v1"));
+app.use("/api/v2", require("./v2"));
 
 // If no version specified, fall back to more current
-app.use("/api", v2);
+app.use("/api", require("./v2"));
 
 // Pretty output
 app.set("json spaces", 2);
 
 
-var port = process.agrv[2] || 5000;
+var port = process.argv[2] || 5000;
 app.listen(port, function() {
   console.log("Listening on port " + port);
 });
